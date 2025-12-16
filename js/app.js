350ed588b2b32e37a308845edc3605959a4b8dae
seragithantsha <tiishetsoseragi@gmail.com>
2025-12-16 17:36:57 +0200

    update

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ const hamburgerList = document.querySelector(".hamburger-list-container");
 hamburgerButton.addEventListener("click", () => {
 	hamburgerList.classList.toggle("hidden");
 });
+
+window.scrollBy({
+	top: 100, // Scrolls down 100 pixels
+	behavior: "smooth",
+});
